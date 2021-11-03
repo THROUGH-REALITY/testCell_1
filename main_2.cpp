fc2b8b1d40f8eaae8b0f81a1aecadd7da6dffb81
Qha3 <93575028+Qha3@users.noreply.github.com>
2021-11-03 22:54:27 +0900

    Add files via upload

--- main_2.cpp
+++ main_2.cpp
@@ -2,30 +2,45 @@
 #include "include/nextCell.hpp"
 #include "include/remaining.hpp"
 #include "include/printCell.hpp"
-#include <stdio.h>
+#include <iostream>
 
-extern int cell[X][Y];
+int studentCount;   //  配置数
+int deskCountX;     //  机x
+int deskCountY;     //  机y
+int allTestCount;   //  試行回数
+int timeCount;      //  世代数
+int existCell;      //  滞在人数 
 
 int main(int argc, char** argv){   //   0,1直接移動
-    for(int testCount = 1; testCount <= 1 /* 試行回数 */; testCount++){
-        int timeCount = 0;  //  世代数
-        int existCell;  //  滞在人数
-        firstCell_1(40);  //  初期配置設定  引数は配置数
+    std::cout << "Type studentCount" << std::endl;
+    std::cin >> studentCount;
+    std::cout << std::endl << "Type deskCountX (dcx <= " << (X - 2) / 2 << ")" << std::endl;
+    std::cin >> deskCountX;
+    std::cout << std::endl << "Type deskCountY (dcy <= " << (Y - 2) / 2 << ")" << std::endl;
+    std::cin >> deskCountY;
+    std::cout << std::endl << "Type allTestCount" << std::endl;
+    std::cin >> allTestCount;
+
+    for(int testCount = 1; testCount <= allTestCount; testCount++){
+        std::cout << std::endl << "test_" << testCount << " start! >>" << std::endl;
+
+        timeCount = 0;  //  世代数
+        firstCell_1(studentCount, deskCountX, deskCountY);  //  初期配置設定  引数は(配置数, 机x{x <= (X - 2) / 2}, 机y{y <= (Y - 2) / 2})
         
-        existCell = remaining_1();    //  調査
+        existCell = remaining_2();    //  調査
         printCell(testCount, timeCount, existCell);     //  記録
-        printf("test_%d start >>%nt == %d, cell == %d%n", testCount, timeCount, existCell);
+        std::cout << "time == " << timeCount << ", existCell == " << existCell << std::endl;
         
         while(existCell > 0){
-            timeCount += 1;  //  世代数
+            timeCount++;  //  世代数
             nextCell_2();    //   配置更新   
             
-            existCell = remaining_1();    //  調査
+            existCell = remaining_2();    //  調査
             printCell(testCount, timeCount, existCell);   //  記録
-            printf("%nt == %d, cell == %d%n", timeCount, existCell);
+            std::cout << "time == " << timeCount << ", existCell == " << existCell << std::endl;
         }
-        printf("test_%d done!%n", testCount);
+        std::cout << "test_" << testCount << " has done!" << std::endl;
     } 
-    printf("all tests have done!");
+    std::cout << "all tests have done!";
     return 0;
 }
